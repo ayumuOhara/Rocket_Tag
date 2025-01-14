e52168453428c053e14b25ab6f1f35bf7a0f8734
ayumuOhara <kmm2459310@stu.o-hara.ac.jp>
2025-01-14 23:52:02 +0900

    プレイヤー/カメラのスクリプト作成

--- Rocket_Tag/Assets/Scripts/PlayerController.cs
+++ Rocket_Tag/Assets/Scripts/PlayerController.cs
@@ -1,16 +1,34 @@
-sing UnityEngine;
+using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    [SerializeField] GameObject player;     // プレイヤーのGameObjectを取得
+    [SerializeField] GameObject cameraObj;  // カメラのGameObjectを取得
+    float moveSpeed;                        // プレイヤーの移動速度
+
     void Start()
     {
-        
+        moveSpeed = 10.0f;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // WASDキーで移動
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Translate(cameraObj.transform.forward * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Translate(cameraObj.transform.forward * -1.0f * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(cameraObj.transform.right * moveSpeed * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(cameraObj.transform.right * -1.0f * moveSpeed * Time.deltaTime);
+        }
     }
 }
