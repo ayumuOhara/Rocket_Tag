b65345af6066190f0affcd2d91381adcfd1e1177
iseukomayatuma <kmm2459305@stu.o-hara.ac.jp>
2025-01-16 14:46:19 +0900

    爆弾時限加速

--- Rocket_Tag/Assets/Scripts/Bomb.cs
+++ Rocket_Tag/Assets/Scripts/Bomb.cs
@@ -3,63 +3,100 @@ using UnityEngine;
 
 public class Bomb : MonoBehaviour
 {
-    float bombLimit = 7.0f;
-    float bombCount = 0;
-    float vibeTime = 10f;
-    float riseSpeed = 60f;
-    float floatingTime = 2f;
+    enum DecreeseLevel
+    {
+        slow,
+        normal,
+        fast
+    }
+
+    DecreeseLevel decreeseLevel = DecreeseLevel.slow;
+
+    //float bombLimit = 1000;
+    //float bombCount = 0;
+    float bombLimit = 0;
+    float bombCount = 1000;
+    float vibeTime = 10;
+    float riseSpeed = 60;
+    float floatingTime = 2;
+    float possesingTime = 0;
+    float[] decreeseValue = { 0.4f, 0.8f, 100f };
+    float[] decreeseUpTime = {5f, 10f, 15f };
+
+
     bool isExplode = false;
 
     Vector3 playerOffset = new Vector3(0, 5, 5);
     Vector3 explodeInpact;
 
     Rigidbody bombRB;
-    GameObject faker;
+    [SerializeField] GameObject player;
     GameObject Camera;
-    Transform fakerTransform;
+    Transform playerTransform;
     Transform cameraTransform;
     void Start()
     {
         bombRB = this.GetComponent<Rigidbody>();
-        faker = GameObject.Find("Faker");    //  ‰¼player
+        player = GameObject.Find("Player");    //  ‰¼player
         Camera = GameObject.Find("Main Camera");
-        fakerTransform = faker.transform;
+        playerTransform = player.transform;
         cameraTransform = Camera.transform;
         explodeInpact = new Vector3(0.2f, cameraTransform.position.y, cameraTransform.position.z);
     }
     void Update()
     {
+      //  Debug.Log(decreeseLevel);
         CountElaps();
-        if((bombLimit < bombCount + 0.5f || isExplode) && vibeTime > 0)
+        if((bombLimit > bombCount - 0.5f || isExplode) && vibeTime > 0)
         {
             ExplodeVibing();
         }
-        if (bombLimit < bombCount)
+        if (bombLimit > bombCount)
         {
             Explosion();
         }
         if(isExplode)
         {
-            fakerTransform.transform.position = bombRB.transform.position + playerOffset;
+            playerTransform.transform.position = bombRB.transform.position + playerOffset;
         }
-        
+        //if(Input.GetKey(KeyCode.Space))
+        //{
+
+        //}
+        DecreeseLevelUp();
     }
     void Explosion()    //  ”š’e”š”­
     {
         Debug.Log(8);
-        bombCount = 0;
         bombRB.linearVelocity = new Vector3(0, riseSpeed, 0);
         isExplode = true;
+        bombCount = 1000;
+        ResetPossesing();
     }
     void CountElaps()    //  Œo‰ß•b”ƒJƒEƒ“ƒg
     {
-        bombCount += Time.deltaTime;
+        bombCount -= (Time.deltaTime + decreeseValue[(int)decreeseLevel] * Time.deltaTime);
+        possesingTime += Time.deltaTime;
+        Debug.Log(bombCount);
     }
     void ExplodeVibing()    //  ”š”­uŠÔ‚ÌƒJƒƒ‰U“®
     {
         cameraTransform.transform.position = explodeInpact;
         explodeInpact.x *= -1;
         vibeTime -= Time.deltaTime;
+    }
+    void DecreeseLevelUp()
+    {
+        if (decreeseLevel != DecreeseLevel.fast && possesingTime > decreeseUpTime[(int)decreeseLevel])
+        {
+            decreeseLevel += 1;
+            Debug.Log(decreeseLevel);
+        }
+    }
+    void ResetPossesing()
+    {
+        possesingTime = 0;
+        decreeseLevel = 0;
     }
 }
 
---
+++ Rocket_Tag/Assets/Scripts/PlayerSecrch.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PlayerSecrch : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
