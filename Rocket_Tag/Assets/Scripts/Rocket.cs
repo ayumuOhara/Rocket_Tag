73dfed7b94a258a34cf326728aea7ebcbef7c547
iseukomayatuma <kmm2459305@stu.o-hara.ac.jp>
2025-01-17 15:12:24 +0900

    ロケット投擲中途

--- Rocket_Tag/Assets/Scripts/Rocket.cs
+++ Rocket_Tag/Assets/Scripts/Rocket.cs
@@ -13,48 +13,55 @@ public class Bomb : MonoBehaviour
     
     DecreeseLevel decreeseLevel = DecreeseLevel.slow;
 
-    float bombLimit = 0;
-    public float bombCount = 1000;
+    float rocketLimit = 0;
+    public float rocketCount = 1000;
     float vibeTime;
     float vibeStartTime = 100f;
     float riseSpeed = 60;
     float floatingTime = 2;
     float floatSpeed = 1f;
-    float throwForce = 100f;
+    float throwForce = 50f;
+    float returnForce = 10f;
     float possesingTime = 0;
+    float playerPosX;
     float[] decreeseValue = { 0.4f, 0.8f, 100f };
     float[] decreeseUpTime = {5f, 10f, 15f };
     bool isExplode = false;
+    bool isThrowing = false;
 
     Vector3 playerOffset = new Vector3(0, 5, 5);
+    Vector3 rocketOffset = new Vector3(0, 5, 5);
     Vector3 explodeInpact;
-    Vector3 thorwDir = Vector3.forward;
 
-    Rigidbody bombRB;
+    Rigidbody rocketRB;
     [SerializeField] GameObject player;
     [SerializeField] GameObject camera;
     GameObject bomb;
     Transform playerTransform;
     Transform cameraTransform;
+
+    Vector3 startpos;
     void Start()
     {
+        Time.timeScale = 0.5f;
         vibeTime = 4;
-        bombRB = this.GetComponent<Rigidbody>();
+        rocketRB = this.GetComponent<Rigidbody>();
         player = GameObject.Find("Player");
         camera = GameObject.Find("Main Camera");
         bomb = GameObject.Find("Bomb");
         playerTransform = player.transform;
         cameraTransform = camera.transform;
         explodeInpact = new Vector3(0.2f, cameraTransform.position.y, cameraTransform.position.z);
+        startpos = this.transform.position;
     }
     void Update()
     {
         CountElaps();
-        if((vibeStartTime > bombCount || isExplode) && vibeTime > 0)
+        if((vibeStartTime > rocketCount || isExplode) && vibeTime > 0)
         {
             CameraVibe();
         }
-        if (bombLimit > bombCount)
+        if (rocketLimit > rocketCount)
         {
             Explosion();
         }
@@ -63,39 +70,40 @@ public class Bomb : MonoBehaviour
             ApproachPos(bomb, player, playerOffset);
         }
         DecreeseLevelUp();
-
         if(Input.GetKeyDown(KeyCode.E))
         {
-            Vector3 screenCenter = new Vector3(Screen.width / 2, Screen.height / 2, 0);
-            Debug.Log(screenCenter);
-            Vector3 worldCenter = Camera.main.ScreenToWorldPoint(screenCenter);
-            Debug.Log(worldCenter);
-            worldCenter.z = camera.transform.position.z * -1 ;
-            Vector3 direction = (worldCenter - transform.position).normalized;
-            bombRB.AddForce(direction * throwForce, ForceMode.Impulse);
-
-            //  bombRB.AddForce(worldCenter.normalized * throwForce, ForceMode.Impulse);
-            // bombRB.linearVelocity = new Vector3(0,0,100);
+            ThrowRocket();
         }
+        //if (Mathf.Abs(rocketRB.position.x - playerPosX) < 2 && isThrowing)
+        //{
+        //    rocketRB.transform.position = player.transform.position + rocketOffset;
+        //}
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            this.transform.position = startpos;
+            rocketRB.linearVelocity = new Vector3(0,0,0);
+        }
+
+        
     }
     void Explosion()    //  爆弾爆発
     {
-        bombRB.useGravity = false;
+        rocketRB.useGravity = false;
         if ((floatingTime -= Time.deltaTime) > 0)
         {
             Floating(transform, floatSpeed);
         }
         else
         {
-            bombRB.linearVelocity = new Vector3(0, riseSpeed, 0);
+            rocketRB.linearVelocity = new Vector3(0, riseSpeed, 0);
             isExplode = true;
-            bombCount = 1000;
+            rocketCount = 1000;
             ResetPossesing();
         }
     }
     void CountElaps()    //  経過秒数カウント
     {
-        bombCount -= Time.deltaTime + decreeseValue[(int)decreeseLevel] * Time.deltaTime;
+        rocketCount -= Time.deltaTime + decreeseValue[(int)decreeseLevel] * Time.deltaTime;
         possesingTime += Time.deltaTime;
     }
     void CameraVibe()    //  ロケット爆発瞬間のカメラ振動
@@ -124,6 +132,38 @@ public class Bomb : MonoBehaviour
     void Floating(Transform floated, float floatForce)    //  オブジェクト浮遊
     {
         floated.position += Vector3.up * floatForce * Time.deltaTime;
+    }
+    Vector3 GetScreenCeterPos()
+    {
+        Vector3 screenCenter = new Vector3(Screen.width / 2, Screen.height / 2, 1000);
+        Vector3 worldCenter = Camera.main.ScreenToWorldPoint(screenCenter);
+        Vector3 direction = (worldCenter - transform.position).normalized;
+        return direction;
+    }
+    void ThrowRocket()
+    {
+        rocketRB.AddForce(GetScreenCeterPos() * throwForce, ForceMode.Impulse);
+        isThrowing = true;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        //bool isFirstCollision = true;
+        //if (isFirstCollision)
+        //{
+        //    isFirstCollision = false;
+        //    return; // 最初の衝突は処理しない
+        //}
+        if (!CompareTag("Player") && !CompareTag("Ground"))
+        {
+            rocketRB.AddForce(GetLineDir() * returnForce, ForceMode.Impulse);
+            Debug.Log(1);
+           
+        }
+    }
+    Vector3 GetLineDir()
+    {
+        Vector3 dir = player.transform.position - this.transform.position;
+        return dir;
     }
     //void BomCouuntDecreese(int value)    //  ロケットカウントを減らす;
     //{
