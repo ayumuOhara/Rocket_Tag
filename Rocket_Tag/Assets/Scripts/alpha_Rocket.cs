dc3bec7fc202c695665df86db8eab429014b66c0
iseukomayatuma <kmm2459305@stu.o-hara.ac.jp>
2025-01-22 12:25:43 +0900

    Rocket & Alpha_Rocket:Refactoring

--- Rocket_Tag/Assets/Scripts/alpha_Rocket.cs
+++ Rocket_Tag/Assets/Scripts/alpha_Rocket.cs
@@ -22,29 +22,28 @@ public class Alpha_Rocket : MonoBehaviourPunCallbacks
     float rocketLimit = 0;
     public float rocketCount = 100;
     public float initialCount = 100;
+    float vibingPower = 0.2f;
+    float vibingDuration = 2;
+    float[] vibeStartTime = { 2, 3.2f, 6, 12, 18, 24, 35 };
     float floatStartTime = 2;
-    float floatSpeed = 2f;
-    float explodeRiseSpeed = 18f;
+    float floatSpeed = 2;
+    float explodeRiseSpeed = 18;
     float possesingTime = 0;
     float secToExplode = 0;
     float evacuateStarPos_Y = 40;
-    float[] decreeseValue = { 0.4f, 1, 1.8f, 5f, 12f, 30f, 100f };
-    float[] decreeseUpTime = { 5f, 10f, 15f, 20f, 25f, 30f, 35f };
-    float[] vibeStartTime = { 2f, 3.2f, 6f, 12f, 18f, 24f,  35f};
+    float[] decreeseValue = { 0.4f, 1, 1.8f, 5, 12, 30, 100 };
+    float[] decreeseUpTime = { 5, 10, 15, 20, 25, 30, 35 };
     bool isExplode = false;
-    bool isVeryHigh = false;
     bool isDropOut = false;
 
     Vector3 startPos;
 
-    GameManager gameManager;
     [SerializeField] GameObject player;
     [SerializeField] GameObject camera;
-    Rigidbody playerRB;
-    Renderer playerRenderer;
-    Material playerMaterial;
-    CameraController cameraController;
     Transform playerTransform;
+    Rigidbody playerRB;
+    GameManager gameManager;
+    CameraController cameraController;
 
     void Start()
     { Initialize(); }
@@ -52,16 +51,30 @@ public class Alpha_Rocket : MonoBehaviourPunCallbacks
     {
         CountElaps();
         if(IsVibeTime())
-        { StartCoroutine(cameraController.Shake(2f, 0.2f)); }
+        { StartCoroutine(cameraController.Shake(vibingDuration, vibingPower)); }
         if (isFloatingTime() && !IsVeryHigh())
         {
             SetGravity(playerRB, false);
+         //   if(!isExplode)
             Floating(playerTransform, floatSpeed);
         }
         if (IsLimitOver() || isExplode)
         { Explosion(); }
         if (decreeseLevel != DecreeseLevel.fastest && possesingTime > decreeseUpTime[(int)decreeseLevel])
         { DecreeseLevelUp(); }
+    }
+    void Initialize()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        Debug.Log(gameManager);
+        playerTransform = player.transform;
+        startPos = playerTransform.position;
+        SetEvacuatePos(40);
+        secToExplode = GetSecUntilZero(rocketCount, (Time.deltaTime + decreeseValue[(int)decreeseLevel] * Time.deltaTime), Time.deltaTime);
+        camera = GameObject.Find("PlayerCamera");     // ゲームプレイで使う
+        playerRB = player.GetComponent<Rigidbody>();
+        cameraController = camera.GetComponent<CameraController>();
+        UpdateRocketCount(rocketCount);
     }
     // ロケットのカウントを全プレイヤーで同期
     float GetSecUntilZero(float limit, float minusValue, float runUnit)    //  ０になるまでの時間を計算(minusValueはrunUnitでの計算後の減少量)
@@ -77,10 +90,21 @@ public class Alpha_Rocket : MonoBehaviourPunCallbacks
         possesingTime += Time.deltaTime;
         UpdateRocketCount(rocketCount);
     }
-    bool IsLimitOver()
+    bool IsVibeTime()    //  カメラ振動時間か判定
+    { return vibeStartTime[(int)decreeseLevel] > rocketCount; }
+    bool isFloatingTime()    //  浮く時間か判定
+    { return floatStartTime > rocketCount; }
+    bool IsVeryHigh()    //  凄い高いか判定
+    { return playerTransform.position.y > evacuateStarPos_Y; }
+    void SetGravity(Rigidbody rB, bool value)    //  RBのuseGravityをセット
+    { rB.useGravity = value; }
+    void Floating(Transform floated, float floatForce)    //  オブジェクト浮遊
+    { floated.position += Vector3.up * floatForce * Time.deltaTime; }
+    bool IsLimitOver()　　　　//  カウントがリミットを下回ったか判定
     { return rocketLimit > rocketCount; }
-    void Explosion()    //  爆弾爆発
+    void Explosion()    //  ロケット爆発
     {
+       // isExplode = true;
         if (!IsVeryHigh())
         {
             Floating(playerTransform, explodeRiseSpeed);
@@ -104,17 +128,6 @@ public class Alpha_Rocket : MonoBehaviourPunCallbacks
             }
         }
     }
-    bool isFloatingTime()
-    { return floatStartTime > rocketCount; }
-    void Floating(Transform floated, float floatForce)    //  オブジェクト浮遊
-    { floated.position += Vector3.up * floatForce * Time.deltaTime; }
-    // ロケットのカウントをリセット
-    void ResetRocketCount()
-    {
-        rocketCount = initialCount; // デフォルト値
-        UpdateRocketCount(rocketCount);
-    }
-
     void DecreeseLevelUp()    //  ロケットカウント加速
     {
         decreeseLevel += 1;
@@ -122,8 +135,12 @@ public class Alpha_Rocket : MonoBehaviourPunCallbacks
         secToExplode = GetSecUntilZero(rocketCount, (Time.deltaTime + decreeseValue[(int)decreeseLevel] * Time.deltaTime), Time.deltaTime);
         //Debug.Log(secToExplode);
     }
-    bool IsVeryHigh()
-    { return playerTransform.position.y > evacuateStarPos_Y; }
+    // ロケットのカウントをリセット
+    void ResetRocketCount()
+    {
+        rocketCount = initialCount; // デフォルト値
+        UpdateRocketCount(rocketCount);
+    }
     public void ResetPossesing()    //  所持における数値の変動リセット
     {
         possesingTime = 0;
@@ -139,27 +156,12 @@ public class Alpha_Rocket : MonoBehaviourPunCallbacks
             rocketCount = (float)changedProps["RocketCount"];
         }
     }
-    bool IsVibeTime()    //  カメラ振動時間か判定
-    { return vibeStartTime[(int)decreeseLevel] > rocketCount; }
-    void SetGravity(Rigidbody rB, bool value)    //  RBのuseGravityをセット
-    { rB.useGravity = value; }
-    void SetEvacuatePos()
-    { evacuateStarPos_Y = startPos.y + 40f; }
-    void Initialize()
-    {
-        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-        Debug.Log(gameManager);
-        playerTransform = player.transform;
-        startPos = playerTransform.position;
-        SetEvacuatePos();
-        secToExplode = GetSecUntilZero(rocketCount, (Time.deltaTime + decreeseValue[(int)decreeseLevel] * Time.deltaTime), Time.deltaTime);
-        camera = GameObject.Find("PlayerCamera");     // ゲームプレイで使う
-        playerRB = player.GetComponent<Rigidbody>();
-        cameraController = camera.GetComponent<CameraController>();
-        UpdateRocketCount(rocketCount);
-    }
+    void SetEvacuatePos(float farFromStartPos)    //  開始位置から一定の距離にあるY座標を代入
+    { evacuateStarPos_Y = startPos.y + farFromStartPos; }
+
     //void ApproachPos(GameObject axis, GameObject Approcher, Vector3 offset)    //  オブジェクトの位置を近づける
     //{
     //    Approcher.transform.position = axis.transform.position + offset;
     //}
 }
+//  floatingとexpolderiseの処理が同時に行われているか検証
