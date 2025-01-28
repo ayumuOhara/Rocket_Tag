571e70436dbc0662b04d663edb6133a0f56b9627
kmm-2459308 <kmm2459308@stu.o-hara.ac.jp>
2025-01-28 15:08:57 +0900

    ギミック作成

--- Rocket_Tag/Assets/Scripts/Gimmick/ObjectBouncer.cs
+++ Rocket_Tag/Assets/Scripts/Gimmick/ObjectBouncer.cs
@@ -5,22 +5,21 @@ public class ObjectBouncer : MonoBehaviour
     // はじく力の大きさ
     public float bounceForce = 5f;
 
-    void OnCollisionEnter(Collision collision)
+    private void OnCollisionEnter(Collision collision)
     {
         // 衝突したオブジェクトがRigidbodyを持っているか確認
         Rigidbody otherRigidbody = collision.rigidbody;
         if (otherRigidbody != null)
         {
-            // 自分の中心から衝突位置へのベクトルを計算
+            // 衝突位置から自分の中心へのベクトルを計算
             Vector3 bounceDirection = collision.transform.position - transform.position;
 
-            // Y軸成分を無視して水平方向の力だけにする
-            bounceDirection.y = 0f;
-            bounceDirection.Normalize(); // 正規化
+            // 水平方向のみに制限
+            bounceDirection.y = 1f;
+            bounceDirection.Normalize(); // 正規化して方向ベクトルを作成
 
             // 水平方向に力を適用
             otherRigidbody.AddForce(bounceDirection * bounceForce, ForceMode.Impulse);
         }
     }
 }
-
