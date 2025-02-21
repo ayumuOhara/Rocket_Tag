e16997687715afcc8d53831258635d54ff54bdd7
kmm-2459355 <kmm2459355@stu.o-hara.ac.jp>
2025-02-21 17:03:18 +0900

    PlayerSkinChange: Complete

--- Rocket_Tag/Assets/Scripts/Locker/PlayerSkin.cs
+++ Rocket_Tag/Assets/Scripts/Locker/PlayerSkin.cs
@@ -10,64 +10,135 @@ public class PlayerSkin : MonoBehaviour    //  プレイヤースキンスクリプト
 {
     enum PlayerSkinNo    //  プレイヤースキン処理一覧
     {
-        DEFAUKT_SKIN,
-        SKIN_NO2,
-        SKIN_NO3,
-        SKIN_NO4,
+        NONE,
+        RED_CAP,
+        STRAW_HAT,
+        ERINGI,
+        FREEZA,
     }
 
-    Button skill;
-    Button cosutume;
-    Button skin0;
-    Button skin1;
-    Button skin2;
-    Button skin3;
+    enum SkinLocation    //  スキンの場所
+    {
+        HEAD,
+        ARM,
+        CHEST,
+        LEG,
+    }
 
-    string sceneName;
+    static GameObject[] skinPrefab;
+    static GameObject skinEntity;
+    Transform headTF;
+    Button redCap;
+    Button strawHat;
+    Button eringi;
+    Button freeza;
+
     int skinNo;
-    bool isCosutume;
+    static int skinLocation;
+
+    static internal GameObject[] _SkinPrefab
+    { get { return _SkinPrefab; } }
+    static internal int _SkinLocation
+    { get { return skinLocation; } }
 
     void Start()
     {
         Initialize();
     }
-    void Update()
-    {
-        cosutume.onClick.AddListener(() =>
-        {
-            isCosutume = true;
-        });
-        skill.onClick.AddListener(() =>
-        {
-            isCosutume = false;
-        });
-        if (isCosutume)
-        {
-            SetSkinNoByButton();
-        }
 
-    }
     void Initialize()     //  初期化
     {
-        skill = GameObject.Find("SkillTabButton").GetComponent<Button>();
-        cosutume = GameObject.Find("CostumeTabButton").GetComponent<Button>();
-        skin0 = GameObject.Find("ccc").GetComponent<Button>();
-        skin1 = GameObject.Find("ddd").GetComponent<Button>();
-        skin2 = GameObject.Find("aaa").GetComponent<Button>();
-        skin3 = GameObject.Find("eee").GetComponent<Button>();
-        skinNo = 0;
+        SceneManager.sceneUnloaded += SaveSkinNo;
+        skinPrefab = new GameObject[5];
+        ResourceLord();
+        headTF     = GameObject.Find("Head"            ).GetComponent<Transform>();
+        redCap     = GameObject.Find("RedCap"          ).GetComponent<Button>();
+        strawHat   = GameObject.Find("StrawHat"        ).GetComponent<Button>();
+        eringi     = GameObject.Find("Eringi"          ).GetComponent<Button>();
+        freeza     = GameObject.Find("Freeza"          ).GetComponent<Button>();
+        SetSkinNoByButton();    //  ボタン反応追加
+
+        skinNo = PlayerPrefs.GetInt("PlayerSkinNo", 0);
+        SkinGenerate(skinLocation);
     }
     void SetSkinNoByButton()    //  ボタン押下に応じて、スキン番号変更関数を呼ぶ
     {
-        skin0.onClick.AddListener(() => ChangeSkinNo((int)PlayerSkinNo.DEFAUKT_SKIN));
-        skin1.onClick.AddListener(() => ChangeSkinNo((int)PlayerSkinNo.SKIN_NO2));
-        skin2.onClick.AddListener(() => ChangeSkinNo((int)PlayerSkinNo.SKIN_NO3));
-        skin3.onClick.AddListener(() => ChangeSkinNo((int)PlayerSkinNo.SKIN_NO4));
+        redCap.onClick.AddListener(() => ChangeSkin((int)PlayerSkinNo.RED_CAP, (int)SkinLocation.HEAD));
+        strawHat.onClick.AddListener(() => ChangeSkin((int)PlayerSkinNo.STRAW_HAT, (int)SkinLocation.HEAD));
+        eringi.onClick.AddListener(() => ChangeSkin((int)PlayerSkinNo.ERINGI, (int)SkinLocation.HEAD));
+        freeza.onClick.AddListener(() => ChangeSkin((int)PlayerSkinNo.FREEZA, (int)SkinLocation.HEAD));
+        Debug.Log(3);
     }
-    void ChangeSkinNo(int clickSkinNo)    //  スキン番号変更
+    void ChangeSkin(int clickSkinNo, int skinLocation)    //  スキン番号変更
     {
-        skinNo = clickSkinNo;
+        if (skinNo != clickSkinNo)
+        {
+            skinNo = clickSkinNo;
+            SkinGenerate(skinLocation);
+        }
+    }
+    void ResourceLord()    //  Resourceフォルダ内のファイルを読み込む
+    {
+        if (skinPrefab[1] == null)
+        {
+            skinPrefab[1] = Resources.Load<GameObject>("RedCap");
+            skinPrefab[2] = Resources.Load<GameObject>("StrawHat");
+            skinPrefab[3] = Resources.Load<GameObject>("Eringi");
+            skinPrefab[4] = Resources.Load<GameObject>("Freeza");
+        }
+    }
+    void SkinGenerate(int skinLocation_)    //  スキンの生成
+    {
+        if(skinNo == 0)
+        {
+            Destroy(skinEntity);
+        }
+        else
+        {
+            switch (skinLocation)
+            {
+                case 0:
+                    {
+                        Destroy(skinEntity);
+                        skinEntity = Instantiate(skinPrefab[skinNo], headTF);
+                        break;
+                    }
+            }
+        }
+        skinLocation = skinLocation_;
+    }
+    void SaveSkinNo(Scene scene)    //  シーンアンロード時にスキンセーブ
+    {
         PlayerPrefs.SetInt("PlayerSkinNo", skinNo);
         PlayerPrefs.Save();
+        Debug.Log(PlayerPrefs.GetInt("PlayerSkinNo"));
+        SceneManager.sceneUnloaded -= SaveSkinNo;
     }
 }
+
+////////////////////////////////////////////  スキン生成方法  //////////////////////////////////
+
+/* 下記の関数を定義して
+ * void SkinGenerate(int skinLocation_)    //  スキンの生成
+    {
+        if(skinNo == 0)
+        {
+            Destroy(skinEntity);
+        }
+        else
+        {
+            switch (skinLocation)
+            {
+                case 0:
+                    {
+                        Destroy(skinEntity);
+                        skinEntity = Instantiate(skinPrefab[skinNo], head);
+                        break;
+                    }
+            }
+        }
+        skinLocation = skinLocation_;
+    }
+ * こいつで生成    SkinGenerate(PlayerSkin._SkinPrefab[PlayerSkin._SkinLocation]);
+   ※※　　HeadのTransformないと生成されません  ※※
+ */
