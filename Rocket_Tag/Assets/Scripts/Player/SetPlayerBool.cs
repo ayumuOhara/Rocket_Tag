a84cba5eaa87990f56bedd37d132f407b989b149
kmm-2459355 <kmm2459355@stu.o-hara.ac.jp>
2025-02-28 14:56:39 +0900

    リザルトの処理をコメントアウト

--- Rocket_Tag/Assets/Scripts/Player/SetPlayerBool.cs
+++ Rocket_Tag/Assets/Scripts/Player/SetPlayerBool.cs
@@ -21,10 +21,10 @@ public class SetPlayerBool : MonoBehaviourPunCallbacks
     private void Start()
     {
         timeManager = GameObject.Find("TimeManager").GetComponent<TimeManager>();
-        resultScreen = GameObject.Find("Result").GetComponent<ResultScreen>();
+        //resultScreen = GameObject.Find("Result").GetComponent<ResultScreen>();
         playerRankManager = GameObject.Find("GameManager").GetComponent<PlayerRankManager>();
-        GameObject result = GameObject.Find("Result");
-        result.SetActive(false);
+        //GameObject result = GameObject.Find("Result");
+        //result.SetActive(false);
     }
 
     // ƒvƒŒƒCƒ„[‚Ìó‘Ô‚Ì‰Šú‰»
@@ -43,8 +43,8 @@ public class SetPlayerBool : MonoBehaviourPunCallbacks
     {
         isDead = newIsDead;
 
-        playerRankManager.SetPlayerRank();
-        resultScreen.ShowMyResult();
+        //playerRankManager.SetPlayerRank();
+        //resultScreen.ShowMyResult();
 
         this.gameObject.SetActive(false);
     }
