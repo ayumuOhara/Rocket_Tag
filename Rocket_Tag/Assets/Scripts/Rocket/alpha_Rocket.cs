ab5c6e600fcb4d8344d7d19b5ed0d13039e91641
ayumuOhara <kmm2459310@stu.o-hara.ac.jp>
2025-03-03 14:11:49 +0900

    Update alpha_Rocket.cs

--- Rocket_Tag/Assets/Scripts/Rocket/alpha_Rocket.cs
+++ Rocket_Tag/Assets/Scripts/Rocket/alpha_Rocket.cs
@@ -39,13 +39,13 @@ public class Alpha_Rocket : MonoBehaviourPunCallbacks
     IEnumerator Explosion()
     {
         Debug.Log("ロケット爆発");
-        float time = 0;
-        while (time < 3.0f)
-        {
-            time += Time.deltaTime;
-            Floating(player, explodeRiseSpeed);
-            yield return null;
-        }
+        //float time = 0;
+        //while (time < 3.0f)
+        //{
+        //    time += Time.deltaTime;
+        //    Floating(player, explodeRiseSpeed);
+        //    yield return null;
+        //}
         DropOut();
         yield break;
     }
