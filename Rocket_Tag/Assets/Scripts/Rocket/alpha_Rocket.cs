b522ceef014835c81886e3f153bdd8fc02c962a7
ayumuOhara <kmm2459310@stu.o-hara.ac.jp>
2025-03-04 14:37:45 +0900

    直って

--- Rocket_Tag/Assets/Scripts/Rocket/alpha_Rocket.cs
+++ Rocket_Tag/Assets/Scripts/Rocket/alpha_Rocket.cs
@@ -83,6 +83,8 @@ public class Alpha_Rocket : MonoBehaviourPunCallbacks
         {
             if (PhotonNetwork.IsMasterClient)  // マスタークライアントが処理
             {
+                // プレイヤーリスト更新後にロケット保持者を選ぶ
+                gameManager.UpdateCachedPlayerList();  // 再度キャッシュを更新
                 gameManager.ChooseRocketPlayer();
             }
         }
@@ -92,5 +94,6 @@ public class Alpha_Rocket : MonoBehaviourPunCallbacks
 
         isExploding = false;
     }
+
 
 }
