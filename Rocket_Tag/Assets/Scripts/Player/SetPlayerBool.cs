4aa5d99c90f2bdf09ea26eea8fc59ee240a3b861
ayumuOhara <kmm2459310@stu.o-hara.ac.jp>
2025-03-10 11:45:07 +0900

    Update SetPlayerBool.cs

--- Rocket_Tag/Assets/Scripts/Player/SetPlayerBool.cs
+++ Rocket_Tag/Assets/Scripts/Player/SetPlayerBool.cs
@@ -55,7 +55,7 @@ public class SetPlayerBool : MonoBehaviourPunCallbacks
 
         if (isDead == true)
         {
-            rocketObj.SetActive(false);
+            this.gameObject.SetActive(false);
         }
     }
 
