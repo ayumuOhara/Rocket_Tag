d306f02edca817dcfd90ff7c64bd23b6180d5fb5
kmm-2459355 <kmm2459355@stu.o-hara.ac.jp>
2025-03-11 14:16:10 +0900

    リザルト修正

--- Rocket_Tag/Assets/Scripts/Result/PlayerRankManager.cs
+++ Rocket_Tag/Assets/Scripts/Result/PlayerRankManager.cs
@@ -25,7 +25,7 @@ public class PlayerRankManager : MonoBehaviour
             return;
         }
 
-        playerRank = gameManager.GetPlayerList().Count;
+        playerRank = gameManager.GetPlayerList().Count +1;
         Debug.Log("Player Rank: " + playerRank);
 
         if (PhotonNetwork.LocalPlayer == null)
@@ -34,24 +34,26 @@ public class PlayerRankManager : MonoBehaviour
             return;
         }
 
+        rankText.text = playerRank.ToString() + "位!";
+
         // 順位をカスタムプロパティに保存
         //Hashtable playerProperties = new Hashtable();
-        playerProperties["PlayerRank"] = playerRank;
-        PhotonNetwork.LocalPlayer.SetCustomProperties(playerProperties);
+        //playerProperties["PlayerRank"] = playerRank;
+        //PhotonNetwork.LocalPlayer.SetCustomProperties(playerProperties);
     }
-    public void ShowMyResult()
-    {
-        int myRank = 1;
+    //public void ShowMyResult()
+    //{
+    //    int myRank = 1;
 
-        //自分の順位をカスタムプロパティから取得
-        if (PhotonNetwork.LocalPlayer.CustomProperties.ContainsKey("PlayerRank"))
-        {
-            Debug.Log((int)PhotonNetwork.LocalPlayer.CustomProperties["PlayerRank"]);
-            myRank = (int)PhotonNetwork.LocalPlayer.CustomProperties["PlayerRank"];
-        }
+    //    //自分の順位をカスタムプロパティから取得
+    //    if (PhotonNetwork.LocalPlayer.CustomProperties.ContainsKey("PlayerRank"))
+    //    {
+    //        Debug.Log((int)PhotonNetwork.LocalPlayer.CustomProperties["PlayerRank"]);
+    //        myRank = (int)PhotonNetwork.LocalPlayer.CustomProperties["PlayerRank"];
+    //    }
 
-        rankText.text = myRank.ToString() + "位!";
-    }
+    //    rankText.text = myRank.ToString() + "位!";
+    //}
 
     // ロビー画面への遷移
     public void LoadLobbyScene()
