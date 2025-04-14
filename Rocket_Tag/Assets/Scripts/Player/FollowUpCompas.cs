4711e89884fb93710281852851be594c928218ee
iseukomayatuma <kmm2459305@stu.o-hara.ac.jp>
2025-04-14 10:12:49 +0900

    Compas: half baked

---
+++ Rocket_Tag/Assets/Scripts/Player/FollowUpCompas.cs
@@ -0,0 +1,14 @@
+using UnityEngine;                                                                          ////  プレイヤーを指し示すコンパスの管理script  ////
+
+public class FollowUpCompas : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
