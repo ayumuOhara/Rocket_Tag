b985614ed6d03170949ac37ab29c819e64e3e1af
iseukomayatuma <kmm2459305@stu.o-hara.ac.jp>
2025-05-21 13:41:33 +0900

    ForComplete half baking

--- Rocket_Tag/Assets/Scripts/Event/EventEffect.cs
+++ Rocket_Tag/Assets/Scripts/Event/EventEffect.cs
@@ -53,7 +53,6 @@ public class EventEffect : MonoBehaviourPunCallbacks                            
     async void Initialize()    //  èâä˙âªä÷êî
     {
         SetDictionary();
-        await LoadEffect();
         assignPlayerTF = AssignPlayersTF;
         teleportSmokeSystem = new ParticleSystem[numOfPlayers];
         spdChagneAuraSystem = new ParticleSystem[numOfPlayers];
