aa6eeb818db375d6daa437c02e939e593bf05bc0
iseukomayatuma <kmm2459305@stu.o-hara.ac.jp>
2025-05-26 11:41:09 +0900

    Rocket Effect Refactoring

--- Rocket_Tag/Assets/Scripts/Event/EventEffect.cs
+++ Rocket_Tag/Assets/Scripts/Event/EventEffect.cs
@@ -21,11 +21,11 @@ public class EventEffect : MonoBehaviourPunCallbacks                            
         SPD_DOWN_AURA,
     }
 
-    IGameMgrFactory Factory;
+    IFactory Factory;
 
     Dictionary<EventEffectProcess, Action> effectProccesMap;
+    static Dictionary<EffectName, GameObject> loadedEffects;
     Dictionary<EffectName, String> effectNameMap;
-    Dictionary<EffectName, GameObject> loadedEffects;
     Action assignPlayerTF;
     GameObject teleportSmokeEntity;
     GameObject spdChagneAuraEntity;
@@ -39,13 +39,12 @@ public class EventEffect : MonoBehaviourPunCallbacks                            
     float defaultPlayerMoveSpd;
     bool isGeneratedSmoke;
     bool isGeneratedSpdAura;
-    bool isGeneratedAllSpdChangeAura;
     const string loadFailMsg = "Load is failed";
     const string teleportSmokeNotGenerate = "Teleport smoke is not loading well";
     const string spdChangingFailMsg = "Spd has not changed";
 
     internal Action _AssignPlayerTF
-    { get { return assignPlayerTF; }}
+    { get { return assignPlayerTF; } }
     internal bool _IsGeneratedSmoke
     { set { isGeneratedSmoke = value; } }                                                       ////  宣言区終了  ////
 
@@ -55,8 +54,8 @@ public class EventEffect : MonoBehaviourPunCallbacks                            
     }                                                                                           ////  処理区終了  ////
     async void Initialize()    //  初期化関数                                                   ////  以下関数区  ////
     {
-        IGameMgrFactory factory = new RealGameMgrFactory();
-        
+        IFactory factory = new RealFactory();
+
         SetDictionary();
         await LoadEffect();
         assignPlayerTF = AssignPlayersTF;
@@ -75,17 +74,17 @@ public class EventEffect : MonoBehaviourPunCallbacks                            
             { EventEffectProcess.MOVE_SPD_AURA, ApplySpdBuffEffect},
             { EventEffectProcess.STOP_SPD_AURA, StopSpdChangeAura}
         };
-        effectNameMap = new Dictionary<EffectName, String>()
-        {
-            {EffectName.TELEPORT_SMOKE, "TeleportSmoke" },
-            {EffectName.SPD_UP_AURA, "SpdUpAura" },
-            {EffectName.SPD_DOWN_AURA, "SpdDownAura" }
-        };
         loadedEffects = new Dictionary<EffectName, GameObject>()
         {
             {EffectName.TELEPORT_SMOKE,null },
             {EffectName.SPD_UP_AURA,   null },
             {EffectName.SPD_DOWN_AURA, null }
+        };
+        effectNameMap = new Dictionary<EffectName, String>()
+        {
+            {EffectName.TELEPORT_SMOKE, "TeleportSmoke" },
+            {EffectName.SPD_UP_AURA, "SpdUpAura" },
+            {EffectName.SPD_DOWN_AURA, "SpdDownAura" }
         };
     }
     async Task LoadEffect()    //  エフェクトロード
@@ -103,18 +102,18 @@ public class EventEffect : MonoBehaviourPunCallbacks                            
 
         foreach (KeyValuePair<EffectName, String> kvp in effectNameMap)
         {
-            KeyValuePair<EffectName, String> kvps = kvp;    //  必要か精査-----------------------------------
-            
-            loadHandle[kvp.Key] = Addressables.LoadAssetAsync<GameObject>(kvps.Value);
-            loadTask.Add(loadHandle[kvps.Key].Task.ContinueWith(t =>    //  読み込みから代入までのタスクを追加する
+            KeyValuePair<EffectName, String> tmpKvp = kvp;    //  必要か精査-----------------------------------
+
+            loadHandle[kvp.Key] = Addressables.LoadAssetAsync<GameObject>(tmpKvp.Value);
+            loadTask.Add(loadHandle[tmpKvp.Key].Task.ContinueWith(t =>    //  読み込みから代入までのタスクを追加する
             {
-                if (loadHandle[kvps.Key].Status == AsyncOperationStatus.Succeeded)
+                if (loadHandle[tmpKvp.Key].Status == AsyncOperationStatus.Succeeded)
                 {
-                    loadedEffects[kvps.Key] = loadHandle[kvps.Key].Result;
+                    loadedEffects[tmpKvp.Key] = loadHandle[tmpKvp.Key].Result;
                 }
                 else
                 {
-                    Debug.LogWarning(loadFailMsg + kvps.Key);    //  デバッグ用--------------------------------------
+                    Debug.LogWarning(loadFailMsg + tmpKvp.Key);    //  デバッグ用--------------------------------------
                 }
             }));
         }
@@ -127,7 +126,7 @@ public class EventEffect : MonoBehaviourPunCallbacks                            
     }
     void HandleTeleportSmoke()    //  テレポート時の煙の制御
     {
-        if(isGeneratedSmoke)
+        if (isGeneratedSmoke)
         {
             for (int i = 0; i < numOfPlayers; i++)
             {
@@ -137,7 +136,7 @@ public class EventEffect : MonoBehaviourPunCallbacks                            
                 }
             }
         }
-        else if(loadedEffects.TryGetValue(EffectName.TELEPORT_SMOKE, out GameObject TeleportSmoke) && TeleportSmoke != null)
+        else if (loadedEffects.TryGetValue(EffectName.TELEPORT_SMOKE, out GameObject TeleportSmoke) && TeleportSmoke != null)
         {
             GenerateTeleportSmoke();
             isGeneratedSmoke = true;
@@ -185,14 +184,14 @@ public class EventEffect : MonoBehaviourPunCallbacks                            
     {
         GetPlayersMovement();
 
-        if (isGeneratedSpdAura && isGeneratedAllSpdChangeAura)
+        if (isGeneratedSpdAura)
         {
             for (int playerIndex = 0; playerIndex < numOfPlayers; playerIndex++)
             {
                 ReplayEffect(spdChagneAuraSystem[playerIndex], Vector3.zero);
             }
         }
-        else if(loadedEffects.TryGetValue(EffectName.TELEPORT_SMOKE, out GameObject TeleportSmoke) && TeleportSmoke != null)
+        else if (loadedEffects.TryGetValue(EffectName.TELEPORT_SMOKE, out GameObject TeleportSmoke) && TeleportSmoke != null)
         {
             GenerateSpdAura();
             isGeneratedSpdAura = true;
@@ -213,7 +212,6 @@ public class EventEffect : MonoBehaviourPunCallbacks                            
     }
     void GenerateSpdAura()    //  スピードを判定して速度エフェクトを出す
     {
-        isGeneratedAllSpdChangeAura = true;
         for (int playerIndex = 0; playerIndex < numOfPlayers; playerIndex++)
         {
             if (playerMovement[playerIndex].GetMoveSpeed() > defaultPlayerMoveSpd)
@@ -228,13 +226,10 @@ public class EventEffect : MonoBehaviourPunCallbacks                            
             }
             else
             {
+                spdChagneAuraEntity = Instantiate(loadedEffects[EffectName.SPD_UP_AURA], players[playerIndex]);
                 Debug.Log(spdChangingFailMsg);    //  デバッグ用--------------------------------------
-                isGeneratedAllSpdChangeAura = false;    //  エラーハンドリング追加---------------------------------------
             }
-            if (isGeneratedAllSpdChangeAura)
-            {
-                spdChagneAuraSystem[playerIndex] = spdChagneAuraEntity.GetComponent<ParticleSystem>();
-            }
+            spdChagneAuraSystem[playerIndex] = spdChagneAuraEntity.GetComponent<ParticleSystem>();
         }
     }
     void StopSpdChangeAura()    //  エフェクトを非表示にする
@@ -244,7 +239,7 @@ public class EventEffect : MonoBehaviourPunCallbacks                            
     }
     void StopEffect(ParticleSystem[] effect)    //  エフェクト一時停止
     {
-        foreach(ParticleSystem p in effect)
+        foreach (ParticleSystem p in effect)
         {
             Debug.Log("Effect Stop loop entire");
             p?.Stop();
@@ -252,65 +247,3 @@ public class EventEffect : MonoBehaviourPunCallbacks                            
     }                                                                                           ////  関数区終了  ////
 }
 // void ReplayEffect(ParticleSystem effect, Vector3 playPos)    //  エフェクト再再生            ////  コード保存場所  ////
-//    {
-//    effect.transform.position = playPos;
-//    effect.Clear();
-//    effect.Play();
-//}
-//[PunRPC]
-//internal void PlayEventEffect(int EffectNo)    //  イベントエフェクトのラッパー関数
-//{
-//    switch (EffectNo)
-//    {
-//        case 0:
-//            {
-//                if (isGeneratedSmoke)
-//                {
-//                    Debug.Log(teleportSmokeSystem);
-
-//                }
-//                else
-//                {
-
-//                    Debug.Log(teleportSmokeSystem);
-//                }
-//                break;
-//            }
-//        default:
-//            {
-//                break;
-//            }
-//    }
-//}
-//[PunRPC]
-//internal void GenerateEffect(int EffectNo, Vector3 players, int playerIndex)    //  エフェクト生成(ラッパー関数)
-//{
-//    Debug.Log("TPエフェクト生成突入");
-//    Debug.Log("TPエフェクト" + teleportSmokePrefab.name);
-
-//    switch (EffectNo)
-//    {
-//        case 0:
-//            {
-//                if (isGeneratedSmoke)
-//                {
-//                    Debug.Log(teleportSmokeSystem);
-//                    teleportSmokeSystem[playerIndex].transform.position = players;
-//                    teleportSmokeSystem[playerIndex].Clear();
-//                    teleportSmokeSystem[playerIndex].Play();
-//                }
-//                else
-//                {
-//                    teleportSmokeEntity = Instantiate(teleportSmokePrefab);
-//                    teleportSmokeEntity.transform.position = players;
-//                    teleportSmokeSystem[playerIndex] = teleportSmokeEntity.GetComponent<ParticleSystem>();
-//                    Debug.Log(teleportSmokeSystem);
-//                }
-//                break;
-//            }
-//        default:
-//            {
-//                break;
-//            }
-//    }
-//}
--- Rocket_Tag/Assets/Scripts/GameManager.cs
+++ Rocket_Tag/Assets/Scripts/GameManager.cs
@@ -19,9 +19,9 @@ public class GameManager : MonoBehaviourPunCallbacks
     [SerializeField] TextMeshProUGUI infoText;          // playerCntTextの説明文
     [SerializeField] GameObject eventTextObj;
     [SerializeField] TextMeshProUGUI eventText;
+    [SerializeField] RocketEffect rocketEffect;           // ロケットエフェクトのインスタンス    for debug--------------------------
 
     //[SerializeField] GameObject rocketEffect;           // ロケットのエフェクト管理オブジェクト
-    //[SerializeField] RocketEffect rocketEffect;           // ロケットエフェクトのインスタンス    for debug--------------------------
     private const int JOIN_CNT_MIN = 2;                 // 参加人数の最小値
     private bool isGameStarted = false;                 // ゲームが開始されたかどうかのフラグ
     private bool hasPlayedCountdownSE = false;          // カウントダウンSEが再生されたかどうかの判定
@@ -114,6 +114,7 @@ public class GameManager : MonoBehaviourPunCallbacks
         {
             ChooseRocketPlayer();
             StartCoroutine(eventManager.TriggerRandomEvent());
+            rocketEffect.Rocket = GameObject.Find("Rocekt").transform;
             //StartCoroutine(CheckOverTime());
             //StartCoroutine(CheckRocketCnt());
         }
