d1f6156ec26a9b0ec7d2552ff11b5684750710c8
ayumuOhara <kmm2459310@stu.o-hara.ac.jp>
2025-05-29 11:40:48 +0900

    【バグ】　スキル使っても足速くならない件について　【スキルとは】

--- Rocket_Tag/Assets/Scripts/Skill/SkillManager.cs
+++ Rocket_Tag/Assets/Scripts/Skill/SkillManager.cs
@@ -11,12 +11,6 @@ public class SkillManager : MonoBehaviourPunCallbacks
 {
     [SerializeField] SkillCoolTime skillCoolTime;
 
-    [SerializeField] SkillDataBase skillDataBase;
-    [SerializeField] public SkillData skillData;
-    public int skillIdx;
-
-    [SerializeField] ObserveDistance observeDistance;
-
     PlayerMovement playerMovement;
     TimeManager timeManager;
     GameManager gameManager;
@@ -30,19 +24,6 @@ public class SkillManager : MonoBehaviourPunCallbacks
 
     float SkillCT = 10.0f;//スキルのクールタイム
 
-    // スキルを設定
-    public void SetSkill(SkillData newSkillData)
-    {
-        skillData = newSkillData;
-        skillIcon.sprite = newSkillData.skillIcon;
-    }
-
-    // 所持スキルを削除
-    public void RemoveSkill()
-    {
-        skillData = null;
-    }
-
     private void Start()
     {
         playerMovement = GetComponent<PlayerMovement>();
@@ -50,9 +31,6 @@ public class SkillManager : MonoBehaviourPunCallbacks
         gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
         skillIcon   = GameObject.Find("SkillIcon").GetComponent<Image>();
         Debug.Log($"SkillIcon：{skillIcon}");
-
-        skillIdx = 4;
-        SetSkill(skillDataBase.SkillData[skillIdx]);
 
         //if (skillCoolTime == null)
         //{
@@ -68,17 +46,7 @@ public class SkillManager : MonoBehaviourPunCallbacks
         {
             //if (skillCoolTime.SkillCool == true)//クールタイム
             //{
-                Debug.Log($"【{skillData.skillName}】を使用");
-
-                switch (skillData.skillId)
-                {
-                    //case 101: break;
-                    case 102: photonView.RPC("PutStickyZone", RpcTarget.All); break;
-                    //case 103: DangerousGift();                            break;
-                    //case 104: SmashPunch(); break;
-                    case 105: StartCoroutine(Dash()); break;
-                }
-
+            StartCoroutine(Dash());
                 //SendSkillData();
 
                 //StartCoroutine(skillCoolTime.CoolTime(SkillCT));//クールタイム
@@ -95,53 +63,11 @@ public class SkillManager : MonoBehaviourPunCallbacks
         zone.transform.position = playerPos;
     }
 
-    // プレイヤーにロケットを配布
-    //void DangerousGift()
-    //{
-    //    int playerCnt = gameManager.GetPlayerList().Count;
-
-    //    int minCnt = 1;
-    //    int maxCnt = playerCnt > 3 ? 3 : playerCnt;
-    //    int rocketCnt = Random.Range(minCnt, maxCnt);
-
-    //    for(int i = 0; i < rocketCnt; i++)
-    //    {
-    //        gameManager.ChooseRocketPlayer();
-    //    }
-    //}
-
-    //// スマッシュパンチ
-    //void SmashPunch()
-    //{
-    //    GameObject target = observeDistance.GetTargetDistance();
-
-    //    if (target == null) return; // ターゲットがいない場合は処理しない
-
-    //    // プレイヤーをターゲットの方向へ向ける
-    //    transform.LookAt(target.transform.position);
-    //    KnockBackTarget(target);
-    //}
-
-    //// 対象を吹っ飛ばす
-    //public void KnockBackTarget(GameObject target)
-    //{
-    //    PhotonView targetView = target.GetComponent<PhotonView>();
-    //    targetView.RPC("SetIsStun", RpcTarget.All, true);
-
-    //    Rigidbody targetRb = target.GetComponent<Rigidbody>();
-    //    if (targetRb != null)
-    //    {
-    //        Vector3 knockbackDirection = (target.transform.position - transform.position).normalized;
-    //        float knockbackForce = 130f;
-
-    //        // 直接 velocity に適用して即座に動かす
-    //        targetRb.linearVelocity = knockbackDirection * knockbackForce;
-    //    }
-    //}
-
     // ダッシュスキル
     IEnumerator Dash()
     {
+        Debug.Log("足が速～い！！！！！！！！！");
+
         float boostValue = 1.5f;     // ダッシュの加速度
         float dashLimit = 3.0f;      // ダッシュの効果時間
 
@@ -157,13 +83,5 @@ public class SkillManager : MonoBehaviourPunCallbacks
         finishSkill = true;
 
         yield break;
-    }
-
-    // スキルをプレイヤーに与える
-    void SendSkillData()
-    {
-        int rnd = Random.Range(0, skillDataBase.SkillData.Count);
-        SkillData giveSkill = skillDataBase.SkillData[rnd];
-        SetSkill(giveSkill);
     }
 }
